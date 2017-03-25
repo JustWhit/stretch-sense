5b8993884b9fb3d1b88230e878884fb8467d626e
Slobodan Milanko <smilanko@Slobodans-MacBook-Pro.local>
2017-03-25 13:09:48 -0400

    Initial Commit

---
+++ StretchSense/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StretchSense
+//
+//  Created by Slobodan Milanko on 3/25/17.
+//  Copyright © 2017 Slobodan Milanko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
