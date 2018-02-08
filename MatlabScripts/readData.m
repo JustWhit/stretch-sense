c94539d52ad934445fb9eed6b8d5530aecc1277d
Jain <32048885+shubham-j@users.noreply.github.com>
2018-02-07 21:52:36 -0500

    Added peak detection to the matlab script

--- MatlabScripts/readData.m
+++ MatlabScripts/readData.m
@@ -1,12 +1,20 @@
 filename = 'Deep_Breaths.csv';
 
+% Read data
 T=readtable(filename);
 
 num = T{:,3};
 cap = T{:,1};
 
+%  Plot data
 figure;
 plot(num,cap)
 title('Deep Breaths')
 xlabel('Sample number');
 ylabel('Capacitance (pF)');
+
+% Find peaks in data
+[pks,locs,widths,proms] = findpeaks(cap, num,'MinPeakDistance',50, 'SortStr','descend');
+plot(num,cap,locs,pks,'o')
+hold on
+text(locs+10,pks,num2str((1:numel(pks))'))
