bf3508e578ccdf4dade345162af47f6f5767a0ee
Justin <jscha002@odu.edu>
2018-03-14 13:16:26 -0400

    matlab script

---
+++ MatlabScripts/current.m
@@ -0,0 +1,211 @@
+
+
+A=[[bike.rmsAx] [bike.rmsAy] [bike.rmsAz] [bike.rmsGx] [bike.rmsGy] [bike.rmsGz] [run.rmsAx] [run.rmsAy] [run.rmsAz] [run.rmsGx] [run.rmsGy] [run.rmsGz] [swim.rmsAx] [swim.rmsAy] [swim.rmsAz] [swim.rmsGx] [swim.rmsGy] [swim.rmsGz] [transition.rmsAx] [transition.rmsAy] [transition.rmsAz] [transition.rmsGx] [transition.rmsGy] [transition.rmsGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Root Mean Square')
+
+A=[[bike.meanAx] [bike.meanAy] [bike.meanAz] [bike.meanGx] [bike.meanGy] [bike.meanGz] [run.meanAx] [run.meanAy] [run.meanAz] [run.meanGx] [run.meanGy] [run.meanGz] [swim.meanAx] [swim.meanAy] [swim.meanAz] [swim.meanGx] [swim.meanGy] [swim.meanGz] [transition.meanAx] [transition.meanAy] [transition.meanAz] [transition.meanGx] [transition.meanGy] [transition.meanGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Mean')
+
+A=[[bike.stddevAx] [bike.stddevAy] [bike.stddevAz] [bike.stddevGx] [bike.stddevGy] [bike.stddevGz] [run.stddevAx] [run.stddevAy] [run.stddevAz] [run.stddevGx] [run.stddevGy] [run.stddevGz] [swim.stddevAx] [swim.stddevAy] [swim.stddevAz] [swim.stddevGx] [swim.stddevGy] [swim.stddevGz] [transition.stddevAx] [transition.stddevAy] [transition.stddevAz] [transition.stddevGx] [transition.stddevGy] [transition.stddevGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Standard Deviation')
+
+A=[[bike.mediAx] [bike.mediAy] [bike.mediAz] [bike.mediGx] [bike.mediGy] [bike.mediGz] [run.mediAx] [run.mediAy] [run.mediAz] [run.mediGx] [run.mediGy] [run.mediGz] [swim.mediAx] [swim.mediAy] [swim.mediAz] [swim.mediGx] [swim.mediGy] [swim.mediGz] [transition.mediAx] [transition.mediAy] [transition.mediAz] [transition.mediGx] [transition.mediGy] [transition.mediGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Median')
+
+
+A=[[bike.avgdisAx] [bike.avgdisAy] [bike.avgdisAz] [bike.avgdisGx] [bike.avgdisGy] [bike.avgdisGz] [run.avgdisAx] [run.avgdisAy] [run.avgdisAz] [run.avgdisGx] [run.avgdisGy] [run.avgdisGz] [swim.avgdisAx] [swim.avgdisAy] [swim.avgdisAz] [swim.avgdisGx] [swim.avgdisGy] [swim.avgdisGz] [transition.avgdisAx] [transition.avgdisAy] [transition.avgdisAz] [transition.avgdisGx] [transition.avgdisGy] [transition.avgdisGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Avg Dis b/ Peaks')
+
+
+A=[[bike.stddisAx] [bike.stddisAy] [bike.stddisAz] [bike.stddisGx] [bike.stddisGy] [bike.stddisGz] [run.stddisAx] [run.stddisAy] [run.stddisAz] [run.stddisGx] [run.stddisGy] [run.stddisGz] [swim.stddisAx] [swim.stddisAy] [swim.stddisAz] [swim.stddisGx] [swim.stddisGy] [swim.stddisGz] [transition.stddisAx] [transition.stddisAy] [transition.stddisAz] [transition.stddisGx] [transition.stddisGy] [transition.stddisGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Std Dev of Dis b/ Peaks')
+
+
+A=[[bike.avgampAx] [bike.avgampAy] [bike.avgampAz] [bike.avgampGx] [bike.avgampGy] [bike.avgampGz] [run.avgampAx] [run.avgampAy] [run.avgampAz] [run.avgampGx] [run.avgampGy] [run.avgampGz] [swim.avgampAx] [swim.avgampAy] [swim.avgampAz] [swim.avgampGx] [swim.avgampGy] [swim.avgampGz] [transition.avgampAx] [transition.avgampAy] [transition.avgampAz] [transition.avgampGx] [transition.avgampGy] [transition.avgampGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Avg Amp of Peaks')
+
+
+A=[[bike.stdampAx] [bike.stdampAy] [bike.stdampAz] [bike.stdampGx] [bike.stdampGy] [bike.stdampGz] [run.stdampAx] [run.stdampAy] [run.stdampAz] [run.stdampGx] [run.stdampGy] [run.stdampGz] [swim.stdampAx] [swim.stdampAy] [swim.stdampAz] [swim.stdampGx] [swim.stdampGy] [swim.stdampGz] [transition.stdampAx] [transition.stdampAy] [transition.stdampAz] [transition.stdampGx] [transition.stdampGy] [transition.stdampGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Std Dev of Amp of Peaks')
+
+
+A=[[bike.skewAx] [bike.skewAy] [bike.skewAz] [bike.skewGx] [bike.skewGy] [bike.skewGz] [run.skewAx] [run.skewAy] [run.skewAz] [run.skewGx] [run.skewGy] [run.skewGz] [swim.skewAx] [swim.skewAy] [swim.skewAz] [swim.skewGx] [swim.skewGy] [swim.skewGz] [transition.skewAx] [transition.skewAy] [transition.skewAz] [transition.skewGx] [transition.skewGy] [transition.skewGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Skewness')
+
+
+A=[[bike.kurtAx] [bike.kurtAy] [bike.kurtAz] [bike.kurtGx] [bike.kurtGy] [bike.kurtGz] [run.kurtAx] [run.kurtAy] [run.kurtAz] [run.kurtGx] [run.kurtGy] [run.kurtGz] [swim.kurtAx] [swim.kurtAy] [swim.kurtAz] [swim.kurtGx] [swim.kurtGy] [swim.kurtGz] [transition.kurtAx] [transition.kurtAy] [transition.kurtAz] [transition.kurtGx] [transition.kurtGy] [transition.kurtGz]]
+labels=["bikeAx", "bikeAy", "bikeAz", "bikeGx", "bikeGy", "bikeGz", "runAx", "runAy", "runAz", "runGx", "runGy", "runGz", "swimAx","swimAy","swimAz", "swimGx" "swimGy", "swimGz", "transAx", "transAy", "transAz", "transGx", "transGy", "transGz"]
+boxplot(A, labels)
+set(gca,'FontSize',10,'XTickLabelRotation',90)
+xlabel('Feature by axis and activity')
+ylabel('Amplitude')
+title('Kurtosis')
+
+%%
+Folder = '/Users/justinschaffner/Desktop/GitRepositories/stretch-sense/Data/SenseAppData/';
+%filename = '/Users/justinschaffner/Desktop/GitRepositories/stretch-sense/Data/SenseAppData/CAP_2018-03-07542162368_U_R_SIDE.csv';
+
+%GTfile='/Users/justinschaffner/Desktop/GitRepositories/stretch-sense/Data/SenseAppData/GT_2018-03-07542161920_U_F_CENTER.csv';
+%SGTfile='/Users/justinschaffner/Desktop/GitRepositories/stretch-sense/Data/SenseAppData/GT_2018-03-07542162176_U_R_CORNER.csv';
+capfiles = ["CAP_2018-03-07542161920_U_F_CENTER.csv","CAP_2018-03-07542162176_U_R_CORNER.csv"];
+GTfiles = ["GT_2018-03-07542161920_U_F_CENTER.csv","GT_2018-03-07542162176_U_R_CORNER.csv"];
+figure; hold on;
+
+for i=1:length(capfiles)
+    capfile=strcat(Folder,capfiles(i));
+    GTfile = strcat(Folder,GTfiles(i));
+    GT=readtable(GTfile);
+    %Read data
+    T=readtable(capfile);
+    
+    Ttime = GT{:,2};
+    Tlabel = GT{:,1};
+    time = T{:,2};
+    cap = T{:,1};
+    start=time(1);
+    for n = 1:length(time)
+       time(n) = time(n)-start;
+    end
+    %for n = 1:length(Ttime)
+    %    Ttime(n) = (100 * (Ttime(n) - floor(Ttime(n)))) + (floor(Ttime(n)) * 60);
+    % end
+
+    start=Ttime(1);
+    for n = 1:length(Ttime)
+        Ttime(n)=Ttime(n)-start;
+    end
+    ts=timeseries(cap,time)
+
+    %  Plot data
+    plot(ts)
+
+    % Find peaks in data
+%     [pks,locs,widths,proms] = findpeaks(cap, time,'MinPeakDistance',2, 'SortStr','descend');
+%     plot(time,cap,locs,pks,'o')
+
+    %z=max(cap{:});
+    %invertedcap= cellfun(@(x) z - x, cap, 'Un', false);
+    %[val,vloc,vwidths,vproms] = findpeaks(invertedcap, time, 'MinPeakDistance',2,'SortStr','descend');
+    % plot(time,cap,vlocs,val,'o')
+    if i==1
+        linetype = 'r--';
+    else
+        linetype = 'b:';
+    end
+    for n=1:length(Ttime)
+%         line([Ttime(n) Ttime(n)],ylim,linetype);
+%         text(Ttime(n),min(ylim),Tlabel(n),'Rotation',90);
+         vline(Ttime(n),linetype,Tlabel(n));
+    end
+
+%     text(locs+10,pks,num2str((1:numel(pks))'))
+    title('Front-Center')
+    xlabel('timestamp');
+    ylabel('Capacitance (pF)');
+
+end
+
+legend('front-center', 'right-corner');
+
+%%
+path = '/Users/justinschaffner/Desktop/GitRepositories/stretch-sense/Data/StretchTesting.csv';
+
+test=readtable(path);
+Idist = test{:,2};
+Icap = test{:,3};
+Ddist = test{:,4};
+Dcap = test{:,5};
+
+IncrD =[0.0,0.0,0.0,0.0];
+IncrC = [0.0,0.0,0.0,0.0];
+Increrr = [0.0,0.0,0.0,0.0];
+DecrD = [0.0,0.0,0.0,0.0];
+DecrC = [0.0,0.0,0.0,0.0];
+Decrerr = [0.0,0.0,0.0,0.0];
+len = length(Idist);
+errlen= len/4;
+for j = 1:4
+    idist = 0.0;
+    icap = 0.0;
+    ddist = 0.0;
+    dcap = 0.0;
+    ivar = zeros(errlen,1);
+    dvar = zeros(errlen,1);
+    count = 1;
+    for i = j:4:len
+        idist = idist + Idist(i);
+        icap = icap + Icap(i);
+        ivar(count) = Icap(i);
+        ddist = ddist + Ddist(i);
+        dcap = dcap + Dcap(i);
+        dvar(count) = Dcap(i);
+        count = count + 1;
+    end    
+    idist = idist / (length(Idist) / 4);
+    IncrD(j)=idist;
+    icap = icap / (length(Icap) / 4);
+    IncrC(j) = icap;
+    Increrr(j)= var(ivar);
+    ddist = ddist / (length(Ddist) / 4);
+    DecrD(j)=ddist;
+    dcap = dcap / (length(Dcap) / 4);
+    DecrC(j)=dcap;
+    Decrerr(j)= var(dvar);
+    
+end
+
+figure; hold on;
+errorbar(IncrD,IncrC, Increrr);
+errorbar(DecrD,DecrC, Decrerr);
+title('Capacitance vs Distance')
+xlabel('Distance(cm)');
+ylabel('Capacitance (pF)');
+legend('Increasing', 'Decreasing');
+
+
+
